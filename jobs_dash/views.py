0011bfb33cb8de02fd642240a36fb8468286d6cc
bryantee <bryantee@gmail.com>
2015-04-13 16:56:10 -0700

    added remaining days on detail view

--- jobs_dash/views.py
+++ jobs_dash/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from jobs_dash.models import Job
+from datetime import datetime
 
 def index(request):
 	jobs_list = Job.objects.order_by('due_date')
-
 	context_dict = {'open_jobs': jobs_list}
+
 	return render(request, 'jobs_dash/index.html', context_dict)
 
 def job_detail(request, job_address_slug):
@@ -22,10 +23,15 @@ def job_detail(request, job_address_slug):
 		context_dict['job_price'] = job.price
 
 		#expirimental - get comments
-		comment_list = job.comment_set.all()
+		comment_list = job.comment_set.all().order_by('date')
 		context_dict['comments'] = comment_list
 
 	except Job.DoesNotExist:
 		pass
+
+	# Calculates days left until project due	
+	today = datetime.today().date()
+	dt = job.due_date - today
+	context_dict['dt'] = dt
 
 	return render(request, 'jobs_dash/job_detail.html', context_dict)
