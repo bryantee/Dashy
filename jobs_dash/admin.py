448caf0cdf39a374cad146e146a6eb5ea77086f5
bryantee <bryantee@gmail.com>
2015-10-05 14:29:23 -0700

    update gitignore, add fieldsets at jobAdmin for readability.

--- jobs_dash/admin.py
+++ jobs_dash/admin.py
@@ -16,6 +16,14 @@ class JobAdmin(admin.ModelAdmin):
 	list_filter = ["due_date"]
 	search_fields = ['address']
 	actions = [make_invoiced]
+	fieldsets = [
+		('House', 			{'fields': ['address', 'due_date', 'price', 'pic']}),
+		('PO\'s', 		{'fields': ['purchase_order', 'repair_po', 'paint_po']}),
+		('Flooring', 	{'fields': ['flooring', 'flooring_date']}),
+		('Agent Info',	{'fields': ['agent_name', 'agent_phone', 'agent_email']}),
+		('Invoice',		{'fields': ['is_open', 'is_invoiced', 'invoiced_date', 'is_paid', 'paid_date']}),
+		(None,			{'fields': ['slug']}),
+	]
 
 
 admin.site.register(Job, JobAdmin)
