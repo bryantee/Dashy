3a2b82b88b619a785fc7ee0934d965a1d12772a3
bryantee <bryantee@gmail.com>
2015-12-08 12:33:30 -0700

    Updated formating for index page 'client'. Uses JS to change background color of each .client box.

--- media/jobs/js/main.js
+++ media/jobs/js/main.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
-	console.log('main.js script loaded')
+	console.log('main.js script loaded') // sanity check
+
+	// hide / show invoiced jobs
 	$('#show-invoiced').on('click', function() {
 		$('.unpaid-invoice-row').toggle("slow", function() {
 			console.log('Show/Hide row');
 		});
-	}) ;
+	});
+
+	// change colors for client background
+	$('.client').each(function() {
+		var text = $(this).text();
+		//console.log(text);
+		if (text == "Charter") {
+			$(this).css("background-color", "#4F9CD4");
+		} else {
+			$(this).css("background-color", "#4CAE4C");
+		} 
+	});
 });
